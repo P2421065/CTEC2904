626c02a631ad65d21500bd88bbeb8c221eaac465
P2421065 <58304516+P2421065@users.noreply.github.com>
2019-11-28 16:32:45 +0000

    Create maths_functions.c

---
+++ Folder_1/maths_functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 9;
+	int y = 3;
+	printf("%i + %i = %i\n", x, y, x+y);
+	printf("%i - %i = %i\n", x, y, x-y);
+	printf("%i * %i = %i\n", x, y, x*y);
+	printf("%i / %i = %i\n", x, y, x/y);
+	return 0;
+}
